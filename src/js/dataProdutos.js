0eeb407bbb6cc04904fe14080a54372ab3cbcc94
Sara <saralins19@gmail.com>
2022-06-10 13:13:27 -0300

    Carrinho de compras atualizado

--- src/js/dataProdutos.js
+++ src/js/dataProdutos.js
@@ -1,58 +1,120 @@
 const produtos = [
     {
+        id: 1,
         nome: 'Banana',
-        preco: 2,
+        preco: '2.00',
         secao: 'Hortifruti',
         categoria: 'fruta',
-        img: "./src/img/banana.png"
+        img: "./src/img/banana.png",
+        promocao: true,
+        precoPromocao: '1.00',
+        componentes: [
+            'Potássio',
+            'Vitamina B6',
+            'Vitamina C',
+            'Folatos'
+        ] 
     },
     {
+        id: 2,
         nome: 'Morango',
-        preco: 2,
+        preco: '2.00',
         secao: 'Hortifruti',
         categoria: 'fruta',
-        img: './src/img/morango.png'
+        img: './src/img/morango.png',
+        promocao: false,
+        precoPromocao: '0',
+        componentes: [
+            'Fibras',
+            'Vitamina C',
+            'Cálcio',
+            'Ferro'
+        ] 
     },
     {
+        id: 3,
         nome: 'Maçã',
-        preco: 2,
+        preco: '2.00',
         secao: 'Hortifruti',
         categoria: 'fruta',
-        img: './src/img/maca.png'
+        img: './src/img/maca.png',
+        promocao: true,
+        precoPromocao: '1.50',
+        componentes: [
+            'Potássio',
+            'Vitamina A',
+            'Vitamina E',
+            'Vitamina C'
+        ] 
     },
     {
+        id: 4,
         nome: 'Pão',
-        preco: 4,
+        preco: '4.00',
         secao: 'Panificadora',
-        categoria: 'Pão',
-        img: './src/img/pao.png'
+        categoria: 'Pães',
+        img: './src/img/pao.png',
+        promocao: true,
+        precoPromocao: '2.50',
+        componentes: [
+            'Cálcio',
+            'Sódio',
+            'Fibra Alimentar',
+            'Proteínas'
+        ] 
     },
     {
+        id: 5,
         nome: 'Leite',
-        preco: 5,
+        preco: '5.00',
         secao: 'Laticínio',
         categoria: 'Bebida',
-        img: './src/img/leite.png'
+        img: './src/img/leite.png',
+        promocao: false,
+        precoPromocao: '1.00',
+        componentes: [
+            'Carboidratos',
+            'Proteínas',
+            'Gorduras totais'
+        ] 
     },
     {
+        id: 6,
         nome: 'Iogurte',
-        preco: 3,
+        preco: "3.00",
         secao: 'Laticínio',
         categoria: 'Bebida',
-        img: './src/img/yorgut.png'
+        img: './src/img/yorgut.png',
+        componentes: [
+            'Carboidratos',
+            'Proteínas',
+            'Gorduras totais'
+        ] 
     },
     {
+        id: 7,
         nome: 'Bolo',
-        preco: 6,
+        preco: "6.00",
         secao: 'Panificadora',
         categoria: 'Doce',
-        img: './src/img/bolo.png'
+        img: './src/img/bolo.png',
+        componentes: [
+            'Carboidratos',
+            'Proteínas',
+            'Gorduras totais'
+        ] 
     },
     {
+        id: 8,
         nome: 'Torta de Maçã',
-        preco: 10,
+        preco: "10.00",
         secao: 'Panificadora',
         categoria: 'Doce',
-        img: './src/img/torta-maca.png'
+        img: './src/img/torta-maca.png',
+        componentes: [
+            'Carboidratos',
+            'Proteínas',
+            'Gorduras totais'
+        ] 
     }
 ]
--- src/js/script.js
+++ src/js/script.js
@@ -6,10 +6,12 @@ function montaCard(product) {
     ul.className = "card"
     
     const li = document.createElement("li");
+    li.className = "li-geral"
     li.appendChild(criaCardHeaderImage(product))
     li.appendChild(criaCardMainTitulo(product))
     li.appendChild(criaCardMainCategoria(product))
-    li.appendChild(criaFooterPreco(product))
+    li.appendChild(criaCardMainInfo(product.componentes))
+    li.appendChild(criarCardFooter(product))
     
     ul.appendChild(li)
 
@@ -64,18 +66,63 @@ function criaCardMainCategoria(product) {
 
 }
 
+function criaCardMainInfo(componentes) {
+
+    const div = document.createElement("div");
+    div.className = "div-info";
+
+    const ul = document.createElement("ul");
+    ul.className = "ul-info"
+
+    componentes.forEach((componente, index) => {
+        const li =  document.createElement("li");
+        li.innerText = `${index+1}. ${componente}`
+        li.className = "li-info"
+
+        ul.append(li)
+        
+    })
+    div.append(ul)
+    return div
+}
+
 function criaFooterPreco(product) {
     
     const divPreco = document.createElement("div");
     divPreco.className = "divPreco";
     
     const p = document.createElement("p");
-    p.innerHTML = `R$${product.preco}.00`;
+    p.innerText = `R$${product.preco}`;
     
     divPreco.appendChild(p);
 
     return divPreco;
 }
+
+function criaFooterButton(product) {
+
+    const divButton = document.createElement("div");
+    divButton.className = "divButton";
+    
+    const button = document.createElement("button");
+    button.className = "button";
+    button.id = product.id;
+    button.innerText = "Comprar";
+    
+    divButton.appendChild(button);
+
+    return divButton;
+}
+
+function criarCardFooter(product) {
+
+    const div = document.createElement("div");
+    div.className = "div-footer"
+    div.append(criaFooterPreco(product),criaFooterButton(product));
+
+    return div;
+}
+
 
 function limpaTela() {
 
@@ -84,22 +131,22 @@ function limpaTela() {
 
 }
 
+function limpaTelacarrinho() {
+
+    const divGeral = document.querySelector(".div-corpo-carrinho");
+    divGeral.innerText = "";
+
+}
+
 function soma(lista) {
 
     let somaTudo = 0;
 
     for(let i = 0; i < lista.length; i++) {
-        somaTudo += lista[i].preco
+        somaTudo += parseInt(lista[i].preco)
     }
 
     return somaTudo;
-}
-
-function alteraPreco(value) {
-
-    const precoAtual = document.querySelector("#precoTotal");
-    precoAtual.innerText = value;
-
 }
 
 function listaCards(arrayObject) {
@@ -107,7 +154,7 @@ function listaCards(arrayObject) {
     limpaTela();
 
     const valorSoma = soma(arrayObject);
-    alteraPreco(valorSoma);
+   // alteraPreco(valorSoma);
 
     for(let i = 0; i < arrayObject.length; i++) {
         montaCard(arrayObject[i]);
@@ -166,8 +213,10 @@ function resultadoValor(value) {
     const arrayApoio = []
 
     if(value.length === arrayApoio.length) {
+        limpaCampo();
         return valorNaoEncontrado();
     }else{
+        limpaCampo();
         return listaCards(value);
     }
 
@@ -180,48 +229,287 @@ function limpaCampo() {
     return campo;
 }
 
-const buttonFruit = document.querySelector('.estiloGeralBotoes--filtrarHortifruti')
-buttonFruit.addEventListener("click", function(event) {
+function montaCarrinho() {
+
+    limpaTelacarrinho()
+    const div = document.querySelector(".div-corpo-carrinho");
+    const i = document.createElement("i");
+    const p = document.createElement("p")
+    const tag = '<i class="fa-solid fa-bag-shopping"></i>';
+    const text = 'Por enquanto não temos produtos no carrinho';
+    p.innerText = text;
+    i.innerHTML = tag;
+    div.append(i,p);
+   
+    return div;
+}
+montaCarrinho()
+
+function criaCardCarrinho(product) {
     
-    if(event.target.innerText.includes("Hortifruti")) {
-        const secao = filtraObjeto(produtos,'Hortifruti');
-        listaCards(secao);
-    }
-})
 
-const buttonLaticinio = document.querySelector('.estiloGeralBotoes--filtrarLaticinio')
-buttonLaticinio.addEventListener("click", function(event) {
+    const divCard = document.createElement("div");
+    divCard.className = "div-card-carrinho";
+
+    const figure = document.createElement("figure");
+    figure.className = "figure-carrinho";
+
+    const img = document.createElement("img");
+    img.className = "img-carrinho";
+    img.src = product.img;
+
+    const divInfo = document.createElement("div");
+    divInfo.className = "divInfo-card-carrinho";
+
+    const pNome = document.createElement("p");
+    pNome.className = "p-nome-carrinho";
+    pNome.innerText = product.nome;
+
+    const pCategoria = document.createElement("p");
+    pCategoria.className = "p-categoria-carrinho";
+    pCategoria.innerText = product.categoria;
+
+    const pPreco = document.createElement("p");
+    pPreco.className = "p-preco-carrinho";
+    pPreco.innerText = product.preco;
+
+    const button = document.createElement("button");
+    button.className = "button-card-carrinho";
+    button.innerHTML = '<i class="fa-solid fa-trash-can"></i>'
+
+    divInfo.append(pNome, pCategoria, pPreco);
+    figure.appendChild(img)
+    divCard.append(figure, divInfo, button);
+
+    return divCard;
+}
+
+let arrayProductNumber = [];
+
+function montaCardCarrinho(product) {
     
-    if(event.target.innerText.includes("Laticínio")) {
-        const secao = filtraObjeto(produtos,'Laticínio');
-        listaCards(secao);
-    }
-})
+    let produto = ""
+    let number = 0
 
-const buttonPanificadora = document.querySelector('.estiloGeralBotoes--filtrarPanificadora')
-buttonPanificadora.addEventListener("click", function(event) {
+    product.forEach((elem, index) => {
+        produtos.filter((elemDataBase) => {
+            if(elemDataBase.id == product[index]) {
+                produto = criaCardCarrinho(elemDataBase)
+                produto.id = index+1;
+                number = number + 1
+            }
+        }) 
+    })
+    arrayProductNumber.push(number)
+    return produto;
+}
+
+function eventFruit() {
+
+    const buttonFruit = document.querySelector('.estiloGeralBotoes--filtrarHortifruti')
+    buttonFruit.addEventListener("click", function(event) {
+        
+        if(event.target.innerText.includes("Hortifruti")) {
+            const secao = filtraObjeto(produtos,'Hortifruti');
+            listaCards(secao);
+        }
+    })
+}
+eventFruit()
+
+function eventLaticinio() {
+
+    const buttonLaticinio = document.querySelector('.estiloGeralBotoes--filtrarLaticinio')
+    buttonLaticinio.addEventListener("click", function(event) {
+        
+        if(event.target.innerText.includes("Laticínio")) {
+            const secao = filtraObjeto(produtos,'Laticínio');
+            listaCards(secao);
+        }
+    })
+}
+eventLaticinio()
+
+function eventPanificadora() {
+
+    const buttonPanificadora = document.querySelector('.estiloGeralBotoes--filtrarPanificadora')
+    buttonPanificadora.addEventListener("click", function(event) {
+        
+        if(event.target.innerText.includes("Panificadora")) {
+            const secao = filtraObjeto(produtos,'Panificadora');
+            listaCards(secao);
+        }
+    })
+}
+eventPanificadora()
+
+function eventButtonTodos() {
+
+    const buttonTodos = document.querySelector('.estiloGeralBotoes--mostrarTodos')
+    buttonTodos.addEventListener("click", function() {
     
-    if(event.target.innerText.includes("Panificadora")) {
-        const secao = filtraObjeto(produtos,'Panificadora');
-        listaCards(secao);
-    }
-})
+        listaCards(produtos);
+        
+    })
+}
+eventButtonTodos()
 
-const buttonTodos = document.querySelector('.estiloGeralBotoes--mostrarTodos')
-buttonTodos.addEventListener("click", function() {
+function eventBusca() {
 
-    listaCards(produtos);
+    const buttonBusca = document.querySelector(".estiloGeralBotoes--botaoBuscaPorNome");
+    buttonBusca.addEventListener("click", function(event) {
     
-})
+        event.preventDefault();
+        const caixaDeTexto = document.querySelector('.campoBuscaPorNome');
+        const valor = caixaDeTexto.value
+        const produtoDigitado = verificaValor(valor);
+        resultadoValor(produtoDigitado);
+    })
+}
+eventBusca()
 
-const buttonBusca = document.querySelector(".estiloGeralBotoes--botaoBuscaPorNome");
-buttonBusca.addEventListener("click", function(event) {
+function eventTecla() {
 
-    event.preventDefault();
-    const caixaDeTexto = document.querySelector('.campoBuscaPorNome');
-    const valor = caixaDeTexto.value
-    const produtoDigitado = verificaValor(valor);
-    resultadoValor(produtoDigitado);
-    limpaCampo();
+    const campo =  document.querySelector(".campoBuscaPorNome");
+    campo.addEventListener("keyup", function(e) {
 
-})
+        if(e.key == "Enter") {
+            const valor = campo.value
+            const produtoDigitado = verificaValor(valor);
+            resultadoValor(produtoDigitado);
+        }
+
+    })
+}
+eventTecla()
+
+let arrayProductId = [];
+
+function montaCardCarrinhoProdutos(divPrincipal, divCapsula, text, produto, arrayTotal) {
+    
+    const divQuantidade = document.createElement("div");
+    divQuantidade.className = "div-quantidade-carrinho";
+    
+    const pTexto = document.createElement("p");
+    pTexto.innerText = text;
+    
+    const pNumber = document.createElement("p");
+    pNumber.className = "p-quantidade";
+    pNumber.innerText = arrayTotal;
+    
+    divQuantidade.append(pTexto, pNumber)
+    divCapsula.append(produto)
+    divPrincipal.append(divCapsula)
+    divPrincipal.append(divQuantidade)
+
+    return divQuantidade
+
+}
+
+function montaTotal(divPrincipal, text) {
+    
+    const divQuantidade = document.createElement("div");
+    divQuantidade.className = "div-quantidade-total";
+    
+    const pTexto = document.createElement("p");
+    pTexto.innerText = text;
+    
+    const pNumber = document.createElement("p");
+    pNumber.className = "p-total";
+    pNumber.innerText = '';
+    
+    divQuantidade.append(pTexto, pNumber)
+    divPrincipal.append(divQuantidade)
+
+    return divQuantidade
+
+}
+
+let prodObject = [];
+
+function eventButtonCompra() {
+
+    const divGeral = document.querySelector(".div-corpo-carrinho");
+
+    const divCapsula = document.createElement("div");
+    divCapsula.className = "div-capsula-carrinho";
+
+    
+    const eventButtonCompra = document.querySelector(".containerListaProdutos");
+    eventButtonCompra.addEventListener("click", function(e) {
+        
+        const valor = e.target;
+        
+        if(valor.className === "button") {
+            
+            limpaTelacarrinho()
+            
+            const productId = e.target.id;
+            arrayProductId.push(productId);
+            montaCardCarrinhoProdutos(divGeral, divCapsula,"Quantidade", montaCardCarrinho(arrayProductId), arrayProductNumber.length);
+            montaTotal(divGeral,"Total");
+            
+            const pNumber = document.querySelector(".p-total");
+            const prod = produtos.find(elem => elem.id == e.target.id)
+            prodObject.push(prod)
+            
+            let soma = 0;
+            prodObject.forEach(elem => soma += parseInt(elem["preco"]))
+            console.log(soma)
+            pNumber.innerText = `${soma}.00`;
+
+        }
+    })
+}
+eventButtonCompra()
+
+function eventButtonRemover() {
+
+    const divCapsula = document.createElement("div");
+    divCapsula.className = "div-capsula-carrinho";
+
+    const eventButtonDelete = document.querySelector(".div-corpo-carrinho");
+    eventButtonDelete.addEventListener("click", function(e) {
+        
+        
+        const valor = e.target;
+
+        if(valor.className === "fa-solid fa-trash-can" || valor.className === "button-card-carrinho") {
+
+            for(let i = 0; i <= arrayProductNumber.length; i++) {
+
+                let eventCardId = e.target.parentElement.parentElement.id;
+                let eventCard = e.target.parentElement.parentElement;
+
+                if(eventCardId == arrayProductNumber[i]) {
+                    let index = arrayProductNumber.indexOf(arrayProductNumber[i])
+                    arrayProductNumber.splice(index, 1);
+                    eventCard.remove()
+
+                    const pNumber = document.querySelector(".p-quantidade");
+                    pNumber.innerText = arrayProductNumber.length;
+
+                    //problema no eventCardId, pois na hora que ele compara um id do database e o id ficticio das divs dos cards do carrinho.
+                    //por isso funciona até certo ponto, depois apresenta problema.
+                    const pTotal = document.querySelector(".p-total");
+
+                    prodObject.forEach(elem => {
+                        let soma = pTotal.innerText;
+                        if(elem.id == eventCardId) {
+                            let index = prodObject.indexOf(prodObject[elem])
+                            prodObject.splice(index, 1);
+                            soma -= elem['preco']
+                            pTotal.innerText = `${soma}.00`;
+                        }
+                    })
+                    
+                    if(arrayProductNumber.length == 0) {
+                        montaCarrinho()
+                    }
+                }
+            }
+        }
+        
+    })
+}
+eventButtonRemover()
